aad20520b87e61c4570bc7a986108d31229dffcd
Pitcherrr <tomhkpitcher@gmail.com>
2023-07-12 14:15:05 +1000

    shapenet to urdf conversion

---
+++ src/active_search/scripts/shapenet_to_urdf.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import rospkg
+from object2urdf import ObjectUrdfBuilder
+from pathlib import Path
+
+# Build entire libraries of URDFs
+rospack = rospkg.RosPack()
+pkg_root = Path(rospack.get_path("active_search"))
+object_folder = pkg_root / "assets/occluding_objs/cap"
+builder = ObjectUrdfBuilder(object_folder)
+builder.build_library(force_overwrite=True, decompose_concave=True, force_decompose=False, center = 'top')
